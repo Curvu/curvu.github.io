67c958d864556575a3b1e93fb90e1d3f6bde1303
Curvu <curvu6@gmail.com>
2023-04-21 15:43:31 +0100

    adding dark mode

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -3,13 +3,18 @@ import './header.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const Header = (props) => {
+  const addDarkMode = () => {
+    const body = document.querySelector('body');
+    body.classList.toggle('dark');
+  }
+
   return (
     <>
       <div className='Header'>
-        <a className='source-tag' href='hello'><i className="bi bi-github"/> Source</a>
+        <a className='source-tag' href='hello'><i className='bi bi-github'/> Source</a>
         <Button onClick={() => window.print()} label='Print' icon='bi bi-printer'/>
         <div className='gap' />
-        <Button onClick={() => console.log('hello, world')} label='Theme' />
+        <Button onClick={addDarkMode} label='Theme' />
       </div>
     </>
   )
